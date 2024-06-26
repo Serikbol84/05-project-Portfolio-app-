65633ce632552a4fcb1909c176c8caf43b81ee46
Serikbol <sh.serikbol@gmail.com>
2024-06-26 19:08:06 +0500

    routers finished

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -9,15 +9,15 @@ const Contacts = () => {
                     <ul className="content-list">
                         <li className="content-list__item">
                             <h2 className="title-2">Location</h2>
-                            <p>Moscow, Russia</p>
+                            <p>Almaty, Kazakhstan</p>
                         </li>
                         <li className="content-list__item">
                             <h2 className="title-2">Telegram / WhatsApp</h2>
-                            <p><a href="tel:+79051234567">+7 (905) 123-45-67</a></p>
+                            <p><a href="tel:+77057063084">+7 (705) 706-30-84</a></p>
                         </li>
                         <li className="content-list__item">
                             <h2 className="title-2">Email</h2>
-                            <p><a href="mailto:webdev@protonmail.com">webdev@protonmail.com</a></p>
+                            <p><a href="mailto:sh.serikbol@gmail.com">sh.serikbol@gmail.com</a></p>
                         </li>
                     </ul>
 
